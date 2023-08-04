ff6adc8983a2f86b482475b4efced513b31aeb41
tanqueta88 <oliverprim783@gmail.com>
2023-08-04 03:27:03 -0300

    function myFunction is added

---
+++ script.js
@@ -0,0 +1,7 @@
+function myFunction() {
+    var usuario = document.getElementById("mail");
+    var password = document.getElementById("psw");
+    
+    var numbers = /^[0-9]+$/;
+    var isnum = /^\d+$/.test(password.value);
+}
