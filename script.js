dbf2a6757491c854bd48398823b0eb6bd53326f6
tanqueta88 <oliverprim783@gmail.com>
2023-08-05 02:23:54 -0300

    Modifications in the myFuncion function

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
 function myFunction() {
     var usuario = document.getElementById("mail");
     var password = document.getElementById("psw");
-    
+
     var numbers = /^[0-9]+$/;
     var isnum = /^\d+$/.test(password.value);
+
+    if (usuario.value == "Sincere@april.biz" && password.value == "81.1496") {
+        window.location.href = "Punto2.html";
+    } else {
+
+        if (alerta.length == 0) {
+            alert("Password y/o contraseña erróneos");
+        } else {
+            alert(alerta);
+        }
+        window.stop();
+    }
 }
